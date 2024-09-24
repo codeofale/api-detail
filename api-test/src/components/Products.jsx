6af3d3ebb9bbfeb48d1150042e0ea81b637aef08
codeofale <xvalee18@gmail.com>
2024-09-24 18:31:33 -0300

    agregamos usos de estados

--- api-test/src/components/Products.jsx
+++ api-test/src/components/Products.jsx
@@ -1,15 +1,32 @@
+import React, { useState, useEffect } from "react";
 function Products() {
-  let products = [];
+  const [products, setProducts] = useState([]);
   /*Pedimos los productos a la api, fake store trae 20 productos para simular ecomm*/
-  fetch("https://fakestoreapi.com/products")
-    .then((res) => res.json())
-    .then((json) => {
-      products = json;
-      console.log(products);
-    });
-  /*controlamos si esta vacio, porque las peticiones son asincronicas*/
-  if (products.length > 0) {
-    products.map((e) => console.log(e));
-  }
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => {
+        setProducts(json);
+      });
+  });
+  return (
+    <>
+      <h1>Lista de Productos</h1>
+      <ul>
+        {products.length === 0 ? (
+          <li>Cargando el producto...</li>
+        ) : (
+          products.map((product) => {
+            return (
+              <li key={product["id"]}>
+                {product["title"]}
+                <article>{product["description"]}</article> <br />
+              </li>
+            );
+          })
+        )}
+      </ul>
+    </>
+  );
 }
 export default Products;
