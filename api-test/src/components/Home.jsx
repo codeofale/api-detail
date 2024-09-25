359fbb18a65e3d174b42c1bb245c014136376563
codeofale <xvalee18@gmail.com>
2024-09-25 16:59:58 -0300

    finish :D

--- api-test/src/components/Home.jsx
+++ api-test/src/components/Home.jsx
@@ -1,9 +1,28 @@
 /*Componente presentacional de los Productos*/
 import Products from "./Products";
+import "../index.css";
+
 function Home() {
   return (
     <>
-      <h1>soy el home </h1>
+      <nav>
+        <div className="navbar">
+          <ul>
+            <li>
+              <a href="#">Inicio</a>
+            </li>
+            <li>
+              <a href="#">Servicios</a>
+            </li>
+            <li>
+              <a href="#">Productos</a>
+            </li>
+            <li>
+              <a href="#">Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
       <Products />
     </>
   );
--- api-test/src/components/Products.jsx
+++ api-test/src/components/Products.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
+import "../index.css";
 function Products() {
   const [products, setProducts] = useState([]);
+  const [mensaje, setMensaje] = useState("");
+  const [comprado, setComprado] = useState({});
   /*Pedimos los productos a la api, fake store trae 20 productos para simular ecomm*/
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
@@ -9,18 +12,44 @@ function Products() {
         setProducts(json);
       });
   });
+  function handleClick(id) {
+    setComprado({ ...comprado, [id]: true });
+    setMensaje("Gracias por su compra");
+    setTimeout(() => {
+      setMensaje("");
+      setComprado({ ...comprado, [id]: false }); // Limpiamos el mensaje después de 3 segundos
+    }, 3000);
+    console.log(comprado);
+  }
   return (
     <>
       <h1>Lista de Productos</h1>
       <ul>
         {products.length === 0 ? (
-          <li>Cargando el producto...</li>
+          <li>Cargando los productos...</li>
         ) : (
           products.map((product) => {
             return (
               <li key={product["id"]}>
-                {product["title"]}
-                <article>{product["description"]}</article> <br />
+                <img
+                  src={product["image"]}
+                  alt="not found"
+                  className="product-img"
+                />
+                <div className="product-info">
+                  <p className="product-name">{product["title"]}</p>
+                  <p className="product-description">
+                    {product["description"]}
+                  </p>
+                  <span className="product-price">${product["price"]}</span>
+                </div>
+                <button
+                  className="buy-btn"
+                  onClick={() => handleClick(product["id"])}
+                >
+                  Comprar
+                </button>
+                {comprado[product["id"]] && <p>{mensaje}</p>}
               </li>
             );
           })
